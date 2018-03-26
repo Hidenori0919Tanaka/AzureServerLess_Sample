23a9d8538f40817649ad6d10d78d7733764bd5cd
HIDENORI TANAKA <hidenori091985@yahoo.co.jp>
2018-03-26 13:39:56 +0900

    TimerLogic

---
+++ BasicWebJobs/BasicWebJobs/Functions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace BasicWebJobs
+{
+    public class Functions
+    {
+#if true
+        public static void TimerFunction([TimerTrigger("0 */5 * * * *")] TimerInfo timerInfo)
+        {
+            Console.WriteLine($"Timer job fired! at {DateTime.Now}");
+        }
+
+        // This function will get triggered/executed when a new message is written 
+        // on an Azure Queue called queue.
+        //public static void ProcessQueueMessage([QueueTrigger("queue")] string message, TextWriter log)
+        //{
+        //    log.WriteLine(message);
+        //}
+#else
+
+#endif
+    }
+}
